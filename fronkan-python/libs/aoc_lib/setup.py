efdb030a5f114ba98231d2a5aa067559908ce430
Fredrik Sjöstrand <fre.sjostrand@gmail.com>
2021-11-28 20:54:45 +0100

    Prepp AoC boilerplate

---
+++ fronkan-python/libs/aoc_lib/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+
+setuptools.setup(
+    name="aoc_lib_2021",
+    version="0.0.1",
+    author="Fredrik Sjöstrand",
+    packages=setuptools.find_packages(),
+    python_requires=">=3.10",
+)
