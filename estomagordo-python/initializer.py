ca8aae5637542a01338c6127ed10c82a51132678
Christofer Ohlsson <christofer.ohlsson@gmail.com>
2021-11-29 21:16:41 +0100

    initialized day 1, added needed newlines

--- estomagordo-python/initializer.py
+++ estomagordo-python/initializer.py
@@ -7,6 +7,8 @@ from functools import reduce
 from heapq import heappop, heappush
 from itertools import combinations, permutations, product
 from helpers import distance, distance_sq, eight_neighs, eight_neighs_bounded, grouped_lines, ints, manhattan, multall, n_neighs, neighs, neighs_bounded
+
+
 def solve(lines):
     pass
 if __name__ == '__main__':
