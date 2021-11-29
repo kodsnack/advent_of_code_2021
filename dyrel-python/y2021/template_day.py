36867624bb31af874eb2e6b6f88e6d39f09e3921
Erik Dyrelius <erik.dyrelius@gmail.com>
2021-11-29 21:14:34 +0100

    Boilerplate up to d01

---
+++ dyrel-python/y2021/template_day.py
@@ -0,0 +1,9 @@
+from common import getPuzzle, submitSecure
+
+puzzle = getPuzzle()
+
+inp = puzzle.input_data
+print(inp)
+# submitSecure(puzzle, "a", "answer a")
+
+# submitSecure(puzzle, "b", "answer b")
