9b72ba52b2353648e57cbfc4919ea35e4f9b1a2f
Christofer Ohlsson <christofer.ohlsson@booking.com>
2021-12-01 11:26:25 +0100

    maybe a bit nicer

--- estomagordo-python/1a.py
+++ estomagordo-python/1a.py
@@ -6,8 +6,8 @@ from itertools import combinations, permutations, product
 from helpers import distance, distance_sq, eight_neighs, eight_neighs_bounded, grouped_lines, ints, manhattan, multall, n_neighs, neighs, neighs_bounded
 
 
-def solve(lines):
-    return sum(lines[x] > lines[x-1] for x in range(1, len(lines)))
+def solve(lines, distance=1):
+    return sum(lines[x] > lines[x-distance] for x in range(distance, len(lines)))
 
 if __name__ == '__main__':
     lines = []
--- estomagordo-python/1b.py
+++ estomagordo-python/1b.py
@@ -6,9 +6,8 @@ from itertools import combinations, permutations, product
 from helpers import distance, distance_sq, eight_neighs, eight_neighs_bounded, grouped_lines, ints, manhattan, multall, n_neighs, neighs, neighs_bounded
 
 
-def solve(lines):    
-    return sum(lines[x] > lines[x-3] for x in range(3, len(lines)))
-    
+def solve(lines, distance=1):
+    return sum(lines[x] > lines[x-distance] for x in range(distance, len(lines)))
 
 if __name__ == '__main__':
     lines = []
@@ -17,4 +16,4 @@ if __name__ == '__main__':
         for line in f.readlines():
             lines.append(int(line))
 
-    print(solve(lines))
+    print(solve(lines, 3))
