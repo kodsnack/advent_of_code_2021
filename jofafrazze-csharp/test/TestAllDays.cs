cbfe6aeb7116de424bb62d3ea6c35c0aa90968be
jofafrazze <johan@hallonbrax.com>
2021-12-01 06:30:15 +0100

    Adding tests

---
+++ jofafrazze-csharp/test/TestAllDays.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace test
+{
+    public class TestDay01
+    {
+        [Fact]
+        public void Test()
+        {
+            Assert.True(day01.Day01.MainTest());
+        }
+    }
+}
