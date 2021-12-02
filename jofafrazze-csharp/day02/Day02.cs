e632429d496acbdfa19305036ebc6a2e86170327
jofafrazze <johan@hallonbrax.com>
2021-12-02 06:17:35 +0100

    Day 02

--- jofafrazze-csharp/day02/Day02.cs
+++ jofafrazze-csharp/day02/Day02.cs
@@ -18,17 +18,66 @@ namespace day02
 
         // Day 02: 
 
+        static int ReadInputA()
+        {
+            StreamReader reader = File.OpenText(inputPath);
+            int d = 0;
+            int f = 0;
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                string[] s = line.Split(' ').ToArray();
+                var i = int.Parse(s[1]);
+                if (s[0][0] == 'd')
+                    d += i;
+                else if (s[0][0] == 'u')
+                    d -= i;
+                else
+                    f += i;
+            }
+            return d * f;
+        }
+
+        static int ReadInputB()
+        {
+            StreamReader reader = File.OpenText(inputPath);
+            int d = 0;
+            int f = 0;
+            int aim = 0;
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                string[] s = line.Split(' ').ToArray();
+                var i = int.Parse(s[1]);
+                if (s[0][0] == 'd')
+                {
+                    aim += i;
+                }
+                else if (s[0][0] == 'u')
+                {
+                    aim -= i;
+                }
+                else
+                {
+                    d += aim * i;
+                    f += i;
+                }
+            }
+            return d * f;
+        }
+
         static Object PartA()
         {
-            var input = ReadIndata.Ints(inputPath);
-            int ans = 0;
+            var input = ReadInputA();
+            int ans = input;
             Console.WriteLine("Part A: Result is {0}", ans);
             return ans;
         }
 
         static Object PartB()
         {
-            int ans = 0;
+            var input = ReadInputB();
+            int ans = input;
             Console.WriteLine("Part B: Result is {0}", ans);
             return ans;
         }
@@ -45,8 +94,8 @@ namespace day02
 
         public static bool MainTest()
         {
-            int a = 42;
-            int b = 4711;
+            int a = 1727835;
+            int b = 1544000595;
             return (PartA().Equals(a)) && (PartB().Equals(b));
         }
     }
