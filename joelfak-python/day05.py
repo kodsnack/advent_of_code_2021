b33a9ade1c8da0731985712bb24dd913cab036e1
Joel Fåk <joel.faak@gmail.com>
2021-12-06 20:46:51 +0100

    Day 5 refactoring

--- joelfak-python/day05.py
+++ joelfak-python/day05.py
@@ -13,23 +13,22 @@ class VentMap():
         self.map = [[0] * size for i in range(size)]
         
         for (start, end) in input:
-            x_range = None
-            y_range = ModuleNotFoundError
+            x_step = 1 if end.x > start.x else -1
+            y_step = 1 if end.y > start.y else -1
             if start.x == end.x:
-                y_range = range(min(start.y,end.y), max(start.y,end.y)+1)
+                y_range = range(start.y, end.y+y_step, y_step)
                 x_range = [start.x] * len(y_range)
             elif start.y == end.y:
-                x_range = range(min(start.x,end.x), max(start.x,end.x)+1)
+                x_range = range(start.x, end.x+x_step, x_step)
                 y_range = [start.y] * len(x_range)
             elif allow_diagonals and (abs(start.x-end.x) == abs(start.y-end.y)):
-                step_x = 1 if end.x > start.x else -1
-                step_y = 1 if end.y > start.y else -1
-                x_range = range(start.x, end.x+step_x, step_x)
-                y_range = range(start.y, end.y+step_y, step_y)
+                x_range = range(start.x, end.x+x_step, x_step)
+                y_range = range(start.y, end.y+y_step, y_step)
+            else:
+                continue
 
-            if x_range:
-                for x, y in zip(x_range, y_range):
-                    self.map[y][x] += 1
+            for x, y in zip(x_range, y_range):
+                self.map[y][x] += 1
 
     def get_map(self):
         return [''.join(map(str, row)).replace('0','.') for row in self.map]
@@ -93,21 +92,26 @@ def test_VentMap_b(input):
                             '1.......1.',
                             '222111....']
 
-def test_VentMap_intersections(input):
+def test_VentMap_intersections_a(input):
     vm = VentMap(input)
     assert vm.get_intersections() == 5
+
+def test_VentMap_intersections_b(input):
+    vm = VentMap(input, True)
+    assert vm.get_intersections() == 12
 
 ## Main ########################################################
 
 if __name__ == '__main__':
     input = []
-    for row in hf.readFile(sys.argv[1]):
-        row = row.split(' -> ')
-        start = Coord(*map(int, row[0].split(',')))
-        end = Coord(*map(int, row[1].split(',')))
-        input.append([start, end])
+    with open(sys.argv[1]) as f:
+        for row in f.readlines():
+            row = row.strip().split(' -> ')
+            start = Coord(*map(int, row[0].split(',')))
+            end = Coord(*map(int, row[1].split(',')))
+            input.append([start, end])
+
 
     print("Advent of code day 5")
-    print("Part1 result: {}".format(part1(input.copy())))
     print("Part1 result: {}".format(part1(input.copy())))
     print("Part2 result: {}".format(part2(input.copy())))
