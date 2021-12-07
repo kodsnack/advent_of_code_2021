f5a540207d9dc49248e3277e932dab6c731ffaa9
Christofer Ohlsson <christofer.ohlsson@booking.com>
2021-12-07 06:10:02 +0100

    as it stood

---
+++ estomagordo-python/day7a.py
@@ -0,0 +1,25 @@
+from collections import Counter, defaultdict, deque
+from functools import reduce
+from heapq import heappop, heappush
+from itertools import combinations, permutations, product
+from helpers import distance, distance_sq, eight_neighs, eight_neighs_bounded, grouped_lines, ints, manhattan, multall, n_neighs, neighs, neighs_bounded, columns
+
+
+def solve(lines):
+    best = (10**10,-1)
+
+    for val in range(min(lines), max(lines)):
+        cost = sum(abs(val-l) for l in lines)
+        best = min(best, (cost,val))
+
+    return best[0]
+
+
+def main():
+    with open('7.txt') as f:
+        for line in f.readlines():
+            return solve(ints(line))
+
+
+if __name__ == '__main__':
+    print(main())
