907f2a3a65a71efc2de7a8023fe2b3b9fd25d52a
jofafrazze <johan@hallonbrax.com>
2021-12-10 10:53:08 +0100

    Day 10 clean up

--- jofafrazze-csharp/day10/Day10.cs
+++ jofafrazze-csharp/day10/Day10.cs
@@ -1,12 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 using AdventOfCode;
-//using Position = AdventOfCode.GenericPosition2D<int>;
 
 namespace aoc
 {
@@ -14,104 +9,40 @@ namespace aoc
     {
         static readonly string day = "day10";
 
-        // Day XX: 
+        // Day 10: Parse lines with different opening and closing brackets
 
-        static int Score(string s)
+        static (int, Stack<char>) Score(string s)
         {
-            var last = new Stack<char>();
-            for (int i = 0; i < s.Length; i++)
+            var stack = new Stack<char>();
+            var copen = "([{<";
+            var cclose = ")]}>";
+            var score = new int[] { 3, 57, 1197, 25137 };
+            foreach (char c in s)
             {
-                char c = s[i];
-                if (c == '(')
-                    last.Push(')');
-                else if (c == '[')
-                    last.Push(']');
-                else if (c == '{')
-                    last.Push('}');
-                else if (c == '<')
-                    last.Push('>');
-                else if (c == last.Peek())
-                    last.Pop();
+                int a = copen.IndexOf(c);
+                if (a >= 0)
+                    stack.Push(cclose[a]);
+                else if (c == stack.Peek())
+                    stack.Pop();
                 else
-                {
-                    char r = c; // last.Pop();
-                    if (r == ')')
-                        return 3;
-                    else if (r == ']')
-                        return 57;
-                    else if (r == '}')
-                        return 1197;
-                    else if (r == '>')
-                        return 25137;
-                }
+                    return (score[cclose.IndexOf(c)], stack);
             }
-            return 0;
+            return (0, stack);
         }
         public static Object PartA(string file)
         {
             var z = ReadInput.Strings(day, file);
-            int sum = 0;
-            foreach (var s in z)
-                sum += Score(s);
-            return sum;
+            return z.Select(x => Score(x).Item1).Sum();
         }
 
-        static long ScoreB(string s)
-        {
-            var last = new Stack<char>();
-            for (int i = 0; i < s.Length; i++)
-            {
-                char c = s[i];
-                if (c == '(')
-                    last.Push(')');
-                else if (c == '[')
-                    last.Push(']');
-                else if (c == '{')
-                    last.Push('}');
-                else if (c == '<')
-                    last.Push('>');
-                else if (c == last.Peek())
-                    last.Pop();
-                else
-                {
-                    char r = c; // last.Pop();
-                    if (r == ')')
-                        return 0;
-                    else if (r == ']')
-                        return 0;
-                    else if (r == '}')
-                        return 0;
-                    else if (r == '>')
-                        return 0;
-                }
-            }
-            long sum = 0;
-            foreach (char c in last)
-            {
-                sum *= 5;
-                if (c == ')')
-                    sum += 1;
-                else if (c == ']')
-                    sum += 2;
-                else if (c == '}')
-                    sum += 3;
-                else if (c == '>')
-                    sum += 4;
-            }
-            return sum;
-        }
         public static Object PartB(string file)
         {
             var z = ReadInput.Strings(day, file);
-            var scores = new List<long>();
-            foreach (var s in z)
-            {
-                long a = ScoreB(s);
-                if (a > 0)
-                    scores.Add(a);
-            }
-            scores.Sort();
-            return scores[scores.Count / 2];
+            var scores = z.Select(s => Score(s)).Where(x => x.Item1 == 0).Select(x => x.Item2).ToList();
+            scores.ForEach(x => x.Push(' '));
+            long f(Stack<char> s) => s.Select(c => (long)" )]}>".IndexOf(c)).Aggregate((a, x) => 5 * a + x);
+            var s = scores.Select(s => f(s)).OrderBy(x => x).ToList();
+            return s[s.Count / 2];
         }
 
         static void Main() => Aoc.Execute(day, PartA, PartB);
---
+++ jofafrazze-csharp/day11/Day11.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using AdventOfCode;
+//using Position = AdventOfCode.GenericPosition2D<int>;
+
+namespace aoc
+{
+    public class Day11
+    {
+        static readonly string day = "day11";
+
+        // Day 11: 
+
+        public static Object PartA(string file)
+        {
+            var z = ReadInput.Ints(day, file);
+            //Console.WriteLine("A is {0}", a);
+            return 0;
+        }
+
+        public static Object PartB(string file)
+        {
+            return 0;
+        }
+
+        static void Main() => Aoc.Execute(day, PartA, PartB);
+    }
+}
