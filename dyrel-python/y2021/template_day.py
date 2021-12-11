c03aea94b3259b9bb5781669f7be787208dd2e68
Erik Dyrelius <erik.dyrelius@gmail.com>
2021-12-11 07:28:39 +0100

    Some template upgrades

--- dyrel-python/y2021/template_day.py
+++ dyrel-python/y2021/template_day.py
@@ -1,10 +1,14 @@
+from re import S
 from common import getPuzzle, submitSecure
-from collections import deque
+from collections import deque, Counter
 from functools import reduce
 from operator import mul
-from itertools import permutations
-from collections import Counter
-from itertools import accumulate
+from itertools import permutations, takewhile, accumulate, count, islice
+
+NEIGHBOUR4 = (-1, -1j, 1, +1j)
+NEIGHBOUR5 = (-1, -1j, 1, +1j, 0)
+NEIGHBOUR8 = (-1-1j, -1j, 1-1j, -1, 1, -1+1j, 1j, 1+1j)
+NEIGHBOUR9 = (-1-1j, -1j, 1-1j, -1, 0, 1, -1+1j, 1j, 1+1j)
 
 puzzle = getPuzzle()
 
