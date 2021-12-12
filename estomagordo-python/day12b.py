f7e963fba9a0426369af2d7d3d007bb29c502adb
Christofer Ohlsson <christofer.ohlsson@booking.com>
2021-12-12 16:39:45 +0100

    actually fast b

--- estomagordo-python/day12b.py
+++ estomagordo-python/day12b.py
@@ -30,6 +30,48 @@ def solve(lines):
         graph[a].append(b)
         graph[b].append(a)
 
+    visited = Counter()
+    stack = [['start', 0]]
+    count = 0
+
+    while stack:
+        node, index = stack.pop()                
+
+        l = len(graph[node])
+
+        if index == l:
+            visited[node] -= 1
+            continue
+
+        following = graph[node][index]
+
+        if following == 'end':
+            count += 1
+            stack.append([node, index+1])
+            continue
+
+        if following == 'start':
+            stack.append([node, index+1])
+            continue
+        
+        if following.isupper():
+            stack.append([node, index+1])
+            stack.append([following, 0])
+        else:
+            if following in visited and visited[following] > 0:
+                if visited.most_common(1)[0][1] == 1:
+                    stack.append([node, index+1])
+                    stack.append([following, 0])
+                    visited[following] += 1
+                else:
+                    stack.append([node, index+1])
+            else:
+                stack.append([node, index+1])
+                stack.append([following, 0])
+                visited[following] += 1
+
+    return count
+
     paths = set()
     frontier = [['start']]
 
