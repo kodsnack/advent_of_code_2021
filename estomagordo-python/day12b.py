4611a1ebdddcd49d8caa3decc95833307e868c8d
Christofer Ohlsson <christofer.ohlsson@booking.com>
2021-12-12 06:21:26 +0100

    cleaner b

--- estomagordo-python/day12b.py
+++ estomagordo-python/day12b.py
@@ -5,6 +5,24 @@ from itertools import combinations, permutations, product
 from helpers import columns, digits, distance, distance_sq, eight_neighs, eight_neighs_bounded, grouped_lines, ints, manhattan, multall, n_neighs, neighs, neighs_bounded
 
 
+def can_add(path, node):
+    if node == 'start':
+        return False
+
+    if node.isupper():
+        return True
+
+    nodecounts = Counter(path)
+
+    if node not in nodecounts:
+        return True
+
+    if nodecounts[node] == 2:
+        return False
+
+    return not any(k.islower() and nodecounts[k] == 2 for k in nodecounts.keys())
+
+
 def solve(lines):
     graph = defaultdict(list)
 
@@ -20,22 +38,11 @@ def solve(lines):
             paths.add(tuple(path))
             continue
 
-        for next in graph[path[-1]]:
-            if next[0].islower() and (next == 'start' or path.count(next) == 2):
+        for node in graph[path[-1]]:
+            if not can_add(path, node):
                 continue
 
-            c = Counter(path + [next])
-
-            cc = 0
-
-            for k in c.keys():
-                if k.islower() and c[k] > 1:
-                    cc += 1
-
-            if cc > 1:
-                continue
-
-            frontier.append(path + [next])
+            frontier.append(path + [node])
 
     return len(paths)
 
