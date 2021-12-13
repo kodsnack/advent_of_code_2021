8ff5d10297f5efa718a3ea701bbec0d2ed2d4d99
jofafrazze <johan@hallonbrax.com>
2021-12-13 11:57:48 +0100

    Day 13 again

--- jofafrazze-csharp/day13/Day13.cs
+++ jofafrazze-csharp/day13/Day13.cs
@@ -12,42 +12,38 @@ namespace aoc
     {
         // Today: Fold paper, do manual OCR 
 
-        static (HashSet<Pos>, List<(bool useX, int n)>) ReadInput(string file)
+        static List<(bool useX, int n)> fold;
+        static HashSet<Pos> ReadData(string file)
         {
             var ps = new HashSet<Pos>();
-            var fs = new List<(bool, int)>();
-            foreach (var s in File.ReadAllLines(AdventOfCode.ReadInput.GetPath(Day, file)))
+            fold = new List<(bool, int)>();
+            foreach (var s in File.ReadAllLines(ReadInput.GetPath(Day, file)))
             {
                 var v = s.Split(',');
-                var w = s.Split(' ').TakeLast(1).ToArray()[0].Split('=');
+                var w = s.Split(' ').Last().Split('=');
                 if (v.Length > 1)
                     ps.Add(new Pos(int.Parse(v[0]), int.Parse(v[1])));
                 else if (w.Length > 1)
-                    fs.Add((w[0] == "x", int.Parse(w[1])));
+                    fold.Add((w[0] == "x", int.Parse(w[1])));
             }
-            return (ps, fs);
+            return ps;
         }
 
-        static HashSet<Pos> FoldPositions(HashSet<Pos> pos, List<(bool useX, int n)> fold)
+        static HashSet<Pos> FoldPositions(HashSet<Pos> pos, bool partA)
         {
             static Pos FoldX(Pos p, int f) => new Pos((p.x > f) ? 2 * f - p.x : p.x, p.y);
-            foreach (var (useX, n) in fold)
+            foreach (var (useX, n) in (partA ? new List<(bool, int)>() { fold[0] } : fold))
                 pos = pos.Select(p => useX ? p : p.SwitchXY()).
                     Where(p => p.x != n).Select(p => FoldX(p, n)).
                     Select(p => useX ? p : p.SwitchXY()).ToHashSet();
             return pos;
         }
 
-        public static Object PartA(string file)
-        {
-            var (pos, fold) = ReadInput(file);
-            return FoldPositions(pos, new List<(bool, int)>() { fold[0] }).Count();
-        }
+        public static Object PartA(string file) => FoldPositions(ReadData(file), true).Count();
 
         public static Object PartB(string file)
         {
-            var (pos, fold) = ReadInput(file);
-            pos = FoldPositions(pos, fold);
+            var pos = FoldPositions(ReadData(file), false);
             Map m = new Map(pos.Select(a => a.x).Max() + 1, pos.Select(a => a.y).Max() + 1, ' ');
             pos.ToList().ForEach(p => m[p] = '#');
             return m.PrintToString();
