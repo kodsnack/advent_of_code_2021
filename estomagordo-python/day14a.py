aab1e37cc04a20fd366b569fce6506e8fe4f62d6
Christofer Ohlsson <christofer.ohlsson@booking.com>
2021-12-14 08:32:58 +0100

    reasonable 14

--- estomagordo-python/day14a.py
+++ estomagordo-python/day14a.py
@@ -5,119 +5,39 @@ from itertools import combinations, permutations, product
 from helpers import columns, digits, distance, distance_sq, eight_neighs, eight_neighs_bounded, grouped_lines, ints, manhattan, multall, n_neighs, neighs, neighs_bounded
 
 
-def solve(chain, transforms, times=40):
-    chain = 'NNCB'
-    chain = 'PHVCVBFHCVPFKBNHKNBO'
-
+def solve(chain, transforms, times=10):
     letters = Counter(chain)
     seen = {}
 
     def helper(pair, remaining):
-        if remaining == 1:
-            for tp, _, middle in transforms:
-                if tp == pair:
-                    return Counter(middle)
+        if remaining == 0:
+            return Counter()
 
         if (pair, remaining) in seen:
             return seen[(pair, remaining)]
 
         for tp, _, middle in transforms:
             if tp == pair:
-                theselets = Counter(middle) + helper(tp[0]+middle, remaining-1) + helper(middle+tp[1], remaining-1)
-                seen[(pair, remaining)] = theselets
-                return theselets
+                result = Counter(middle) + helper(tp[0]+middle, remaining-1) + helper(middle+tp[1], remaining-1)
+                seen[(pair, remaining)] = result
+                return result
 
     for x in range(1, len(chain)):
         letters += helper(chain[x-1]+chain[x], times)
-    print(letters)
-    return letters.most_common(1)[0][1] - letters.most_common(len(letters))[-1][1]
     
-    pairs = []
-
-    for x in range(1, len(chain)):
-        pairs.append(chain[x-1]+chain[x])
-
-    seen = {}
-
-    def helper(pair, remaining, letter):
-        if remaining == 0:
-            return pair.count(letter)
-            for tp, _, middle in transforms:
-                if tp == pair:
-                    return (pair+middle).count(letter)
-
-        if (pair, remaining, letter) in seen:
-            return seen[(pair, remaining, letter)]
-
-        count = 0
-
-        for tp, _, middle in transforms:
-            if tp == pair:
-                count = helper(pair[0] + middle, remaining-1, letter) + helper(middle+pair[1], remaining-1, letter)#//(2 if remaining == 1 else 1)
-                break
-
-        seen[(pair, remaining, letter)] = count
-
-        return count
-
-    lettersums = []
-    alletters = set(chain)
-    # pairs = ['NC']
-    for _, _, let in transforms:
-        alletters.add(let)
-    print(alletters)
-    for letter in alletters:
-        total = 0#-chain[1:-1].count(letter)
-        for pair in pairs:
-            v = helper(pair, times, letter)
-            total += v - (v - pair.count(letter))//2
-        lettersums.append(total)
-        print(letter, total)
-
-    lettersums.sort()
-    print(lettersums)
-    # return lettersums[-1]-lettersums[0]
-
-    for step in range(times):
-        newchain = []
-
-        for x in range(1, len(chain)):
-            ab = chain[x-1]+chain[x]
-            found = False
-
-            for pair, _, middle in transforms:
-                if pair == ab:
-                    if x == 1:
-                        newchain.append(ab[0] + middle + ab[1])
-                    else:
-                        newchain.append(middle + ab[1])                    
-                    found = True
-                    break
-            
-            if not found:
-                newchain.append(ab)
-
-        chain = ''.join(newchain)
-        c = Counter(chain)
-        n = len(c)
-        print(step, c)
-
-        print(step, c.most_common(1)[0][1], c.most_common(n)[-1][1], c.most_common(1)[0][1] - c.most_common(n)[-1][1])
-
-
-    c = Counter(chain)
-    n = len(c)
-    print(c)
-    return c.most_common(1)[0][1] - c.most_common(n)[-1][1]
+    return letters.most_common(1)[0][1] - letters.most_common(len(letters))[-1][1]
 
 
 def main():
-    chain = 'PHVCVBFHCVPFKBNHKNBO'
+    chain = ''
     transforms = []
 
     with open('14.txt') as f:
         for line in f.readlines():
-            transforms.append(line.split())
+            if not chain:
+                chain = line.rstrip()
+            elif line.rstrip():
+                transforms.append(line.split())
             
     return solve(chain, transforms)
 
--- estomagordo-python/day14b.py
+++ estomagordo-python/day14b.py
@@ -5,18 +5,41 @@ from itertools import combinations, permutations, product
 from helpers import columns, digits, distance, distance_sq, eight_neighs, eight_neighs_bounded, grouped_lines, ints, manhattan, multall, n_neighs, neighs, neighs_bounded
 
 
-def solve(lines):
-    pass
+def solve(chain, transforms, times=10):
+    letters = Counter(chain)
+    seen = {}
+
+    def helper(pair, remaining):
+        if remaining == 0:
+            return Counter()
+
+        if (pair, remaining) in seen:
+            return seen[(pair, remaining)]
+
+        for tp, _, middle in transforms:
+            if tp == pair:
+                result = Counter(middle) + helper(tp[0]+middle, remaining-1) + helper(middle+tp[1], remaining-1)
+                seen[(pair, remaining)] = result
+                return result
+
+    for x in range(1, len(chain)):
+        letters += helper(chain[x-1]+chain[x], times)
+    
+    return letters.most_common(1)[0][1] - letters.most_common(len(letters))[-1][1]
 
 
 def main():
-    lines = []
+    chain = ''
+    transforms = []
 
     with open('14.txt') as f:
         for line in f.readlines():
-            lines.append(line)
+            if not chain:
+                chain = line.rstrip()
+            elif line.rstrip():
+                transforms.append(line.split())
             
-    return solve(lines)
+    return solve(chain, transforms, 40)
 
 
 if __name__ == '__main__':
