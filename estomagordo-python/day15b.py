43869000cf4b59579332fdb0733691cb05840a92
Christofer Ohlsson <christofer.ohlsson@booking.com>
2021-12-15 06:24:29 +0100

    cant get reasonable b to work, regroup?

---
+++ estomagordo-python/day15b.py
@@ -0,0 +1,58 @@
+from collections import Counter, defaultdict, deque
+from functools import reduce
+from heapq import heappop, heappush
+from itertools import combinations, permutations, product
+from helpers import chunks, chunks_with_overlap, columns, digits, distance, distance_sq, eight_neighs, eight_neighs_bounded, grouped_lines, ints, manhattan, multall, n_neighs, neighs, neighs_bounded
+
+
+def solve(lines):
+    # def transform(n):
+    #     m = n+1
+    #     if m == 10:
+    #         m = 1
+    #     return m
+
+    # newlines = [list(line) for line in lines]
+
+    # for line in lines:
+    #     newline = list(map(transform, line))
+
+    height = len(lines)
+    width = len(lines[0])
+
+    frontier = [(0, 0, 0)]
+    seen = {(0, 0): 0}
+
+    while True:
+        score, y, x = heappop(frontier)
+        bigy = y // height
+        bigx = x // width
+        smally = y % height
+        smallx = x % width
+
+        if y == height*5-1 and x == width*5-1:
+            return score
+
+        for ny, nx in neighs_bounded(y, x, 0, height*5-1, 0, width*5-1):
+            neighscore = lines[smally][smallx] + bigy + bigx
+            if neighscore > 9:
+                neighscore -= 9
+
+            newscore = score+neighscore
+            if (ny, nx) not in seen or seen[(ny, nx)] > newscore:
+                seen[(ny, nx)] = newscore
+                heappush(frontier, (newscore, ny, nx))
+
+
+def main():
+    lines = []
+
+    with open('15.txt') as f:
+        for line in f.readlines():
+            lines.append(digits(line))
+            
+    return solve(lines)
+
+
+if __name__ == '__main__':
+    print(main())
