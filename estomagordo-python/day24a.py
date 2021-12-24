3adff51cb6024aa5e1fc2674c4353791b3e81812
Christofer Ohlsson <christofer.ohlsson@gmail.com>
2021-12-24 20:03:02 +0100

    iterative

--- estomagordo-python/day24a.py
+++ estomagordo-python/day24a.py
@@ -81,103 +81,26 @@ def solve(instructions):
 
     programettes = list(chunks(instructions, 18))
     n = len(programettes)
-    seen = set()
-    count = 0
-    goalcount = 0
-    lastreached = -1
-    found = {(0, 0, 0, 0)}
+    seen = {(0, 0, 0, 0)}
 
-    def helper(i, x, y, z, word):
-        nonlocal count, goalcount, lastreached
+    frontier = deque([(0, 0, 0, 0, '')])
 
-        lastreached = max(lastreached, i)
-
-        count += 1
-
-        if count % 10**6 == 0:
-            print(count, goalcount, lastreached, len(seen))
+    while True:
+        i, x, y, z, word = frontier.popleft()
+        if len(seen) % 10**6 == 0:
+            print(len(seen), len(frontier))
 
         if i == n-1:
             for w in range(1, 10):
                 _, __, dz = calcfor(w, x, y, z, programettes[i][1:])
                 if dz == 0:
-                    found.add(word + str(w))
-                    print(word + str(w))
-                    goalcount += 1
-            return
+                    return word + str(w)
         
         for w in range(9, 0, -1):
             dx, dy, dz = calcfor(w, x, y, z, programettes[i][1:])
             if (i+1, dx, dy, dz) not in seen:
                 seen.add((i+1, dx, dy, dz))
-                helper(i+1, dx, dy, dz, word+str(w))
-
-    helper(0, 0, 0, 0, '')
-
-    return sorted(found)
-
-    # outcomes = {}
-
-    # for i, chunk in enumerate(chunks(instructions, 18)):
-    #     print(i, len(outcomes))
-    #     chunkout = defaultdict(int)
-
-    #     if i == 0:
-    #         for w in range(1, 10):                
-    #             retx, rety, retz = calcfor(w, 0, 0, 0, chunk)
-    #             chunkout[(w, 0, 0, 0)] = (retx, rety, retz)
-    #     else:
-    #         for x, y, z in outcomes.values():
-    #             for w in range(1, 10):
-    #                 retx, rety, retz = calcfor(w, x, y, z, chunk)
-    #                 chunkout[(retx, rety, retz)] = (retx, rety, retz)
-
-    #     outcomes = chunkout
-    
-    # return max(oc[0] for oc in outcomes if oc[-1] == 0)
-
-    # res = ''
-    
-    # for chunk in chunks(instructions, 18):
-    #     for val in range(9, 0, -1):
-    #         if isvalid(val, chunk):
-    #             res += str(val)
-    #             break
-
-    # return ''.join(res)
-    
-    largest = 0
-    lo = 11111111111111
-    hi = 99999999999999
-
-    # for num in range(hi, 0, -1):
-    #     if not '0' in str(num):
-    #         res = isvalid(num)
-    #         if not res:
-    #             print(num)
-
-    # while lo < hi:
-    #     mid = (lo+hi)//2
-    for mid in (11912814611156, 21912814611157, 31912814611158, 41912814611159):
-        if '0' in str(mid):
-            print('zeroes', mid)    
-            s = str(mid)
-
-            for pos in range(len(s)):
-                if s[pos] == '0':
-                    s = s[:pos] + '1' + s[pos+1:]
-
-            mid = int(s)
-
-        if isvalid(mid, instructions):
-            largest = max(largest, mid)
-            print('best', mid)
-            lo = mid+1
-        else:
-            print('fails', mid)
-            hi = mid-1
-
-    return largest
+                frontier.append((i+1, dx, dy, dz, word+str(w)))
 
 
 def main():
@@ -192,8 +115,3 @@ def main():
 
 if __name__ == '__main__':
     print(main())
-
-# 11912814611156 wrong-
-# 41912814611159 wrong
-# 31912814611158 not tried
-# 21912814611157 not tried
