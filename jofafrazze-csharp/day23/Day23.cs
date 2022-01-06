75dec815024bfb7c2db1b652dcfa9edb62962d8b
jofafrazze <johan@hallonbrax.com>
2022-01-06 16:24:43 +0100

    Replacing APos with Pos again, Making Amps a member of the State struct

--- jofafrazze-csharp/day23/Day23.cs
+++ jofafrazze-csharp/day23/Day23.cs
@@ -9,63 +9,45 @@ namespace aoc
     {
         // Amphipod: Play game moving all amphipods back home
 
-        record struct APos(byte x, byte y) : IComparable<APos>
-        {
-            public APos(Pos p) : this((byte)p.x, (byte)p.y) { }
-            public APos(char x, char y) : this((byte)(x - 'E'), (byte)(y - 'E')) { }
-            public int CompareTo(APos a) => (y != a.y) ? (y < a.y ? -1 : 1) : (x == a.x ? 0 : x < a.x ? -1 : 1);
-        }
-        record struct Amp(char c, APos p) : IComparable<Amp>
+        record struct Amp(char c, Pos p) : IComparable<Amp>
         {
             public Amp(Amp a) : this(a.c, a.p) { }
-            public Amp(string s) : this(s[0], new APos(s[1], s[2])) { }
+            public Amp(string s) : this(s[0], new Pos(s[1] - 'E', s[2] - 'E')) { }
             public int CompareTo(Amp a) => p.CompareTo(a.p);
             public string Str() => new StringBuilder().Append(c).Append((char)('E' + p.x)).Append((char)('E' + p.y)).ToString();
         }
-        record struct State(string Id)
+        record struct State(string Id, List<Amp> Amps)
         {
-            public State(List<Amp> amps) : this(FromAmps(amps)) { }
-            public static string FromAmps(List<Amp> amps)
+            public State(List<Amp> amps) : this(CreateId(amps), amps) { }
+            public static string CreateId(List<Amp> amps)
             {
                 var sb = new StringBuilder();
-                amps.OrderBy(w => w).ToList().ForEach(w => sb.Append(w.Str()));
+                amps.ForEach(w => sb.Append(w.Str()));
                 return sb.ToString();
             }
-            public static List<Amp> Replace(ref State s, APos from, APos to)
+            public static List<Amp> Replace(List<Amp> amps, Pos from, Pos to)
             {
-                var amps = s.AllAmps();
                 for (int i = 0; i < amps.Count; i++)
                     if (amps[i].p == from)
                     {
                         amps[i] = new Amp(amps[i]) { p = to };
                         break;
                     }
+                amps.Sort();
                 return amps;
             }
+            public bool Equals(State other) => Id.Equals(other.Id);
+            public override int GetHashCode() => Id.GetHashCode();
             public bool Done()
             {
                 for (int i = 0; i < Id.Length; i += 3)
                 {
                     char c = Id[i];
-                    byte x = (byte)(Id[i + 1] - 'E');
+                    int x = (int)(Id[i + 1] - 'E');
                     if ((c == 'A' && x != 3) || (c == 'B' && x != 5) || (c == 'C' && x != 7) || (c == 'D' && x != 9))
                         return false;
                 }
                 return true;
-            }
-            public List<Amp> AllAmps()
-            {
-                var res = new List<Amp>(Id.Length / 3);
-                for (int i = 0; i < Id.Length; i += 3)
-                    res.Add(new Amp(Id.Substring(i, 3)));
-                return res;
-            }
-            public List<APos> AllAPos()
-            {
-                var res = new List<APos>(Id.Length / 3);
-                for (int i = 0; i < Id.Length; i += 3)
-                    res.Add(new APos(Id[i + 1], Id[i + 2]));
-                return res;
             }
             public bool Evacuating(int x, char c)
             {
@@ -106,14 +88,14 @@ namespace aoc
             public string PrintToString(Map m0)
             {
                 Map m = new(m0);
-                foreach (var amp in AllAmps())
+                foreach (var amp in Amps)
                     m.data[amp.p.x, amp.p.y] = amp.c;
                 return m.PrintToString();
             }
             public string PrintToCompactString(int index, int score)
             {
                 int maxDepth = Id.Length / 12;
-                var map = AllAmps().ToDictionary(w => w.p, w => w.c);
+                var map = Amps.ToDictionary(w => w.p, w => w.c);
                 var sb = new StringBuilder();
                 sb.AppendFormat("{0,2}: ", index);
                 for (int x = 1; x <= 11; x++)
@@ -122,11 +104,11 @@ namespace aoc
                     {
                         sb.Append('[');
                         for (int y = 2; y <= maxDepth + 1; y++)
-                            sb.Append(map.GetValueOrDefault(new APos((byte)x, (byte)y), '.'));
+                            sb.Append(map.GetValueOrDefault(new Pos(x, y), '.'));
                         sb.Append(']');
                     }
                     else
-                        sb.Append(map.GetValueOrDefault(new APos((byte)x, 1), '.'));
+                        sb.Append(map.GetValueOrDefault(new Pos(x, 1), '.'));
                 }
                 return sb.AppendFormat(": {0,5}", score).ToString();
             }
@@ -142,7 +124,7 @@ namespace aoc
                     evac[xhome[c]] = e;
                     distHome[xhome[c]] = maxDepth - (e ? 0 : AmpsHome(xhome[c], c));
                 }
-                foreach (var a in AllAmps())
+                foreach (var a in Amps)
                     if (!(IsXHome(a.p.x) && !evac[a.p.x]))
                         cost += (a.p.y - 1 + Math.Abs(a.p.x - xhome[a.c]) + distHome[xhome[a.c]]--) * mult[a.c];
                 return cost;
@@ -164,14 +146,13 @@ namespace aoc
             gameStates.Enqueue(Id3(m), 0);
             var gamesPlayed = new Dictionary<State, (int score, State? prev)>() { [Id3(m)] = (0, null) };
             m.Positions().Where(p => !" #.".Contains(m[p])).ToList().ForEach(p => m[p] = '.');
-            int mDist(APos p1, APos p2) => Math.Abs(p1.x - p2.x) + Math.Abs(p1.y - p2.y);
-            List<(APos, int)> Reachable(ref State state, Amp start)
+            List<(Pos, int)> Reachable(ref State state, Amp start)
             {
-                var playersPos = state.AllAPos().ToHashSet();
-                var canReach = new List<(APos p, int s)>();
-                void GoDownHome(APos p, int dist)
+                var playersPos = state.Amps.Select(w => w.p).ToHashSet();
+                var canReach = new List<(Pos p, int s)>();
+                void GoDownHome(Pos p, int dist)
                 {
-                    APos nextp = p;
+                    Pos nextp = p;
                     nextp.y += 1;
                     while (nextp.y <= ymax && !playersPos!.Contains(nextp))
                     {
@@ -181,22 +162,22 @@ namespace aoc
                     canReach!.Add((p, dist + p.y - 1));
                 }
                 bool gotHome = false;
-                void GoSideways(APos p, int dir, bool stopInHallway, int xhome = -1)
+                void GoSideways(Pos p, int dir, bool stopInHallway, int xhome = -1)
                 {
                     int end = dir < 0 ? 0 : 12;
-                    p.x += (byte)dir;
+                    p.x += dir;
                     while (p.x != end && !playersPos!.Contains(p))
                     {
                         if (stopInHallway && (p.x <= 2 || p.x >= 10 || p.x % 2 == 0))
-                            canReach!.Add((p, mDist(start.p, p)));
+                            canReach!.Add((p, p.ManhattanDistance(start.p)));
                         if (p.x == xhome)
                         {
                             canReach!.Clear();
-                            GoDownHome(p, mDist(start.p, p));
+                            GoDownHome(p, p.ManhattanDistance(start.p));
                             gotHome = true;
                             return;
                         }
-                        p.x += (byte)dir;
+                        p.x += dir;
                     }
                 }
                 int sxhome = xhome[start.c];
@@ -206,7 +187,7 @@ namespace aoc
                     GoSideways(start.p, homeDir, false, sxhome);
                 else if (start.p.y > 1)
                 {
-                    APos p = new APos(start.p.x, 1);
+                    Pos p = new Pos(start.p.x, 1);
                     GoSideways(p, homeDir, true, canGoHome ? sxhome : -1);
                     if (!gotHome)
                         GoSideways(p, homeDir > 0 ? -1 : 1, true, -1);
@@ -216,7 +197,7 @@ namespace aoc
             State Id3(Map m)
             {
                 var pos = m.Positions().Where(w => "ABCD".Contains(m[w]));
-                return new State(pos.Select(p => new Amp(m[p], new APos(p))).ToList());
+                return new State(pos.Select(p => new Amp(m[p], p)).OrderBy(w => w).ToList());
             }
             int nNewGames = 0;
             int nBetterGames = 0;
@@ -228,9 +209,10 @@ namespace aoc
                 foreach (var amp in toMove)
                 {
                     var r = Reachable(ref state, amp);
-                    foreach ((APos p, int s) in r)
+                    foreach ((Pos p, int s) in r)
                     {
-                        State newState = new(State.Replace(ref state, amp.p, p));
+                        var newAmps = new List<Amp>(state.Amps);
+                        State newState = new(State.Replace(newAmps, amp.p, p));
                         int newScore = score + s * mult![amp.c];
                         bool playedBefore = gamesPlayed!.ContainsKey(newState);
                         if (!playedBefore || gamesPlayed[newState].score > newScore)
@@ -262,14 +244,14 @@ namespace aoc
                     break;
                 else
                 {
-                    bool keepOn = true;
-                    while (keepOn)
-                    {
-                        var movingIn = state.AllAmps().Where(w => w.p.y == 1);
-                        keepOn = MovePlayer(movingIn, ref state, score);
-                    }
-                    //var movingIn = state.AllAmps().Where(w => w.p.y == 1);
-                    //MovePlayer(movingIn, ref state, score);
+                    //bool keepOn = true;
+                    //while (keepOn)
+                    //{
+                    //    var movingIn = state.Amps.Where(w => w.p.y == 1);
+                    //    keepOn = MovePlayer(movingIn, ref state, score);
+                    //}
+                    var movingIn = state.Amps.Where(w => w.p.y == 1);
+                    MovePlayer(movingIn, ref state, score);
                     var movingOut = state.MovingOut();
                     MovePlayer(movingOut, ref state, score);
                 }
